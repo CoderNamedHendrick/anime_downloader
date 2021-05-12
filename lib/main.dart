6b91c2c31518b267203301e279b101fbcfb19b04
Hendrick <sebastinesoacatp@gmail.com>
2021-05-12 09:37:50 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
 import 'package:anime_downloader/services/api_service.dart';
+import 'package:anime_downloader/services/desc_json_to_dart.dart';
+import 'package:anime_downloader/services/download_links_json_to_dart.dart';
+import 'package:anime_downloader/services/episodes_json_to_dart.dart';
+import 'package:anime_downloader/services/search_json_to_dart.dart';
 import 'package:flutter/material.dart';
 
 
 void main(){
 
-  // ApiService.instance.search(name: 'world trigger');
-  // ApiService.instance.desc(link: '/category/world-trigger-dub');
-  // ApiService.instance.episodes(start: '1', end: '12', id: '8933');
-  ApiService.instance.downloadLink(link: '/world-trigger-dub-episode-2');
+   // Future<List<Search>> result = ApiService.instance.search(name: 'world trigger');
+   // result.then((value) {
+   //   List<Search> search = value.toList();
+   //   for(int i = 0; i<search.length; i++){
+   //     print(search[i].name);
+   //     print(search[i].link);
+   //   }
+   // });
+  Future<Desc> description = ApiService.instance.desc(link: '/category/world-trigger-dub');
+  description.then((value) {
+    Desc desc = value;
+    print(desc.name);
+    print(desc.summary);
+    print(desc.genre);
+  });
+  // Future<List<Episodes>> result = ApiService.instance.episodes(start: '1', end: '12', id: '8933');
+  //  result.then((value) {
+  //    List<Episodes> search = value.toList();
+  //    for(int i = 0; i<search.length; i++){
+  //      print(search[i].name);
+  //      print(search[i].link);
+  //    }
+  //  });
+  // Future<List<DownloadLink>> result = ApiService.instance.downloadLink(link: '/world-trigger-dub-episode-2');
+  // result.then((value) {
+  //   List<DownloadLink> search = value.toList();
+  //   for(int i = 0; i<search.length; i++){
+  //     print(search[i].name);
+  //     print(search[i].link);
+  //   }
+  // });
   runApp(Home());
 }
 
