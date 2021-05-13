9f7c1aac2301af83a2a2aab0f2c2548c471f7157
Hendrick <sebastinesoacatp@gmail.com>
2021-05-13 22:01:10 +0100

    refactoring - moving to BLoC   - description bloc

---
+++ lib/blocs/description_bloc.dart
@@ -0,0 +1,36 @@
+
+import 'dart:async';
+
+import 'package:anime_downloader/repository/description_repository.dart';
+import 'package:anime_downloader/services/api_response.dart';
+
+class DescriptionBloc{
+  DescriptionRepository _descriptionRepository;
+
+  StreamController _descriptionController;
+
+  StreamSink<ApiResponse<DescriptionModel>> get descriptionSink => _descriptionController.sink;
+
+  Stream<ApiResponse<DescriptionModel>> get descriptionStream => _descriptionController.stream;
+
+  DescriptionBloc({String link}){
+    _descriptionController = StreamController<ApiResponse<DescriptionModel>>();
+    _descriptionRepository = DescriptionRepository();
+    fetchDescription(link);
+  }
+
+  fetchDescription(String link) async{
+    descriptionSink.add(ApiResponse.loading('Fetching Description'));
+    try{
+      DescriptionModel desc = await _descriptionRepository.fetchDesc(link: link);
+      descriptionSink.add(ApiResponse.completed(desc));
+    } catch(e){
+      descriptionSink.add(ApiResponse.error(e.toString()));
+      print(e);
+    }
+  }
+
+  dispose(){
+    _descriptionController?.close();
+  }
+}
