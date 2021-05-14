5a9dcc18190f469c7c78a81a8df10a998700dd19
Hendrick <sebastinesoacatp@gmail.com>
2021-05-14 21:58:51 +0100

    working implementation - .

--- lib/main.dart
+++ lib/main.dart
@@ -7,38 +7,6 @@ import 'package:flutter/material.dart';
 
 
 void main(){
-
-   // Future<List<Search>> result = ApiService.instance.search(name: 'world trigger');
-   // result.then((value) {
-   //   List<Search> search = value.toList();
-   //   for(int i = 0; i<search.length; i++){
-   //     print(search[i].name);
-   //     print(search[i].link);
-   //   }
-   // });
-  // Future<Desc> description = ApiService.instance.desc(link: '/category/world-trigger-dub');
-  // description.then((value) {
-  //   Desc desc = value;
-  //   print(desc.name);
-  //   print(desc.summary);
-  //   print(desc.genre);
-  // });
-  // Future<List<Episodes>> result = ApiService.instance.episodes(start: '1', end: '12', id: '8933');
-  //  result.then((value) {
-  //    List<Episodes> search = value.toList();
-  //    for(int i = 0; i<search.length; i++){
-  //      print(search[i].name);
-  //      print(search[i].link);
-  //    }
-  //  });
-  // Future<List<DownloadLink>> result = ApiService.instance.downloadLink(link: '/world-trigger-dub-episode-2');
-  // result.then((value) {
-  //   List<DownloadLink> search = value.toList();
-  //   for(int i = 0; i<search.length; i++){
-  //     print(search[i].name);
-  //     print(search[i].link);
-  //   }
-  // });
   runApp(Home());
 }
 
