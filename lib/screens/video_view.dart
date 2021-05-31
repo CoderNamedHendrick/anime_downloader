dc52c7611635d90851305bd9f62d805959e4320c
Hendrick <sebastinesoacatp@gmail.com>
2021-05-31 01:14:16 +0100

    video playing

---
+++ lib/screens/video_view.dart
@@ -0,0 +1,59 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoView extends StatefulWidget {
+  final VideoPlayerController videoPlayerController;
+  final bool looping;
+  final bool autoplay;
+
+  VideoView(
+      {@required this.videoPlayerController,
+      this.looping,
+      this.autoplay,
+      Key key})
+      : super(key: key);
+
+  @override
+  _VideoViewState createState() => _VideoViewState();
+}
+
+class _VideoViewState extends State<VideoView> {
+  ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _chewieController = ChewieController(
+      videoPlayerController: widget.videoPlayerController,
+      aspectRatio: 5/8,
+      autoInitialize: true,
+      autoPlay: widget.autoplay,
+      looping: widget.looping,
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Text(
+            errorMessage,
+            style: TextStyle(color: Colors.white),
+          ),
+        );
+      }
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _chewieController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Chewie(
+        controller: _chewieController,
+      ),
+    );
+  }
+}
