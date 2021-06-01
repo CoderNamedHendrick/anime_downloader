f1f024ebc802f14336c6d1a76962b631fa5404df
Hendrick <sebastinesoacatp@gmail.com>
2021-06-01 13:39:53 +0100

    popular api calls and ui

--- lib/repository/description_repository.dart
+++ lib/repository/description_repository.dart
@@ -1,5 +1,7 @@
 import 'package:anime_downloader/model/description_model.dart';
+import 'package:anime_downloader/model/popular_model.dart';
 import 'package:anime_downloader/services/api_service.dart';
+import 'package:flutter/material.dart';
 
 class DescriptionRepository {
   ApiService _service = ApiService();
@@ -7,5 +9,10 @@ class DescriptionRepository {
   Future<DescriptionModel> fetchDesc({String link}) async {
     final response = await _service.get('/desc?link=$link');
     return DescriptionModel.fromJson(response);
+  }
+
+  Future<List<PopularModel>> fetchPopular() async {
+    final response = await _service.get('/popular');
+    return List<PopularModel>.from(response.map((e) => PopularModel.fromJson(e)));
   }
 }
