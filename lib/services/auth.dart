5dd8d5073ab958e08c014f9618f9a0b2482a6d21
Hendrick <sebastinesoacatp@gmail.com>
2021-06-30 20:59:21 +0100

    more fixes

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,3 @@
-import 'package:anime_downloader/keys.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:twitter_login/twitter_login.dart';
@@ -70,8 +69,8 @@ class Auth implements AuthBase {
   @override
   Future<User> signInWithTwitter() async {
     final twitterLogin = TwitterLogin(
-      apiKey: CONSUMER_KEY,
-      apiSecretKey: CONSUMER_KEY_SECRET,
+      apiKey: 'imJr3lVDEHUatITQkLwBoPwZs',
+      apiSecretKey: 'Q90mEFWSoTlJVAEfk6JVqIs9E4xcHxqCqTOj3lGuBBfiDQKqjc',
       redirectURI: 'example://',
     );
     final authResult = await twitterLogin.login();
